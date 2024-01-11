96a8a94fc2edf5952ea26d3f389b6eda287874e7
Bocabbage <378066505@qq.com>
2024-01-11 22:36:18 +0800

    Update load dotenv logic.

--- mikanani/src/__init__.py
+++ mikanani/src/__init__.py
@@ -1,4 +1,7 @@
 from dotenv import load_dotenv, find_dotenv
 
 # load dotenv file
-load_dotenv(find_dotenv(), override=True)
+try:
+    load_dotenv(find_dotenv(raise_error_if_not_found=True), override=True)
+except Exception as e:
+    load_dotenv("/secret/.env", override=True)
