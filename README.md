0e7e80a6828cbedb497a7fbe42ad9c46cdf5a45f
Bocabbage <378066505@qq.com>
2024-02-27 18:07:40 +0800

    Add grpc-generated codes.

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 
 ## Mikanani
 Anime-downloader based on MikanAni-RSS subscription and Qbittorrent.
+### gRPC-related
+```shell
+python -m grpc_tools.protoc \
+-I./mikanani/protos \
+--python_out=./mikanani/src/grpc \
+--pyi_out=./mikanani/src/grpc \
+--grpc_python_out=./mikanani/src/grpc \
+./mikanani/protos/mongodb_crud.proto
+```
---
+++ mikanani/src/grpc/mongodb_crud_pb2.pyi
@@ -0,0 +1,50 @@
+from google.protobuf.internal import containers as _containers
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Iterable as _Iterable, Optional as _Optional
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class QueryAnimeRequest(_message.Message):
+    __slots__ = ("names",)
+    NAMES_FIELD_NUMBER: _ClassVar[int]
+    names: _containers.RepeatedScalarFieldContainer[str]
+    def __init__(self, names: _Optional[_Iterable[str]] = ...) -> None: ...
+
+class QueryAnimeResponse(_message.Message):
+    __slots__ = ("names", "rssUrl")
+    NAMES_FIELD_NUMBER: _ClassVar[int]
+    RSSURL_FIELD_NUMBER: _ClassVar[int]
+    names: _containers.RepeatedScalarFieldContainer[str]
+    rssUrl: _containers.RepeatedScalarFieldContainer[str]
+    def __init__(self, names: _Optional[_Iterable[str]] = ..., rssUrl: _Optional[_Iterable[str]] = ...) -> None: ...
+
+class UpdateAnimeRequest(_message.Message):
+    __slots__ = ("names", "rssUrl", "rssVersion", "rssRegex")
+    NAMES_FIELD_NUMBER: _ClassVar[int]
+    RSSURL_FIELD_NUMBER: _ClassVar[int]
+    RSSVERSION_FIELD_NUMBER: _ClassVar[int]
+    RSSREGEX_FIELD_NUMBER: _ClassVar[int]
+    names: _containers.RepeatedScalarFieldContainer[str]
+    rssUrl: _containers.RepeatedScalarFieldContainer[str]
+    rssVersion: _containers.RepeatedScalarFieldContainer[str]
+    rssRegex: _containers.RepeatedScalarFieldContainer[str]
+    def __init__(self, names: _Optional[_Iterable[str]] = ..., rssUrl: _Optional[_Iterable[str]] = ..., rssVersion: _Optional[_Iterable[str]] = ..., rssRegex: _Optional[_Iterable[str]] = ...) -> None: ...
+
+class UpdateAnimeResponse(_message.Message):
+    __slots__ = ("successCount",)
+    SUCCESSCOUNT_FIELD_NUMBER: _ClassVar[int]
+    successCount: int
+    def __init__(self, successCount: _Optional[int] = ...) -> None: ...
+
+class DelAnimeRequest(_message.Message):
+    __slots__ = ("names",)
+    NAMES_FIELD_NUMBER: _ClassVar[int]
+    names: _containers.RepeatedScalarFieldContainer[str]
+    def __init__(self, names: _Optional[_Iterable[str]] = ...) -> None: ...
+
+class DelAnimeResponse(_message.Message):
+    __slots__ = ("successCount",)
+    SUCCESSCOUNT_FIELD_NUMBER: _ClassVar[int]
+    successCount: int
+    def __init__(self, successCount: _Optional[int] = ...) -> None: ...
