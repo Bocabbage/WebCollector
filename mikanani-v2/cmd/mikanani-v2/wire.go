f1f61e0d4c2e56881253562b031a54766873716c
Bocabbage <378066505@qq.com>
2024-04-28 09:59:31 +0800

    Init for mikanani-v2.

---
+++ mikanani-v2/cmd/mikanani-v2/wire.go
@@ -0,0 +1,23 @@
+//go:build wireinject
+// +build wireinject
+
+// The build tag makes sure the stub is not built in the final build.
+
+package main
+
+import (
+	"mikanani-v2/internal/biz"
+	"mikanani-v2/internal/conf"
+	"mikanani-v2/internal/data"
+	"mikanani-v2/internal/server"
+	"mikanani-v2/internal/service"
+
+	"github.com/go-kratos/kratos/v2"
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/google/wire"
+)
+
+// wireApp init kratos application.
+func wireApp(*conf.Server, *conf.Data, log.Logger) (*kratos.App, func(), error) {
+	panic(wire.Build(server.ProviderSet, data.ProviderSet, biz.ProviderSet, service.ProviderSet, newApp))
+}
